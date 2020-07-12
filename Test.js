8c8bb64e8c456debb9403efae1ae9654b2e72aba
M <matthewbye01@gmail.com>
2020-07-13 00:49:29 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -5,10 +5,13 @@ function hide(){
 	document.getElementById("Date").innerHTML = ""
 }
 
-function showFile(selectedFile){
+var filename = function showFile(selectedFile){
 	var selectedFile = document.getElementById('fileInput').files;
 	document.getElementById("demo").innerHTML = selectedFile[0].name;
 	console.log(selectedFile);
 	console.log(selectedFile[0].name)
+	const reader = new FileReader()
+	var textFromFile = reader.readAsText(selectedFile[0].name);
+	console.log(textFromFile)
+	document.getElementById("fileContents").innerHTML = textFromFile
 }
-console.log(selectedFile);
