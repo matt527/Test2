afa98e8b92dca9096e139c45ac2d968889d58333
M <matthewbye01@gmail.com>
2020-07-13 12:52:56 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -32,7 +32,4 @@ function convert(fileContents){
 	console.log(dataList[0])
 }
 
-var map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
-});
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
