72b984217309403861e927f5837da20542458a33
M <matthewbye01@gmail.com>
2020-07-13 17:10:59 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -15,16 +15,16 @@ function fileAdded(){
   reader.readAsText(file)
   reader.onload = function(){
 	document.getElementById("filename").innerHTMl = filename  
-  	document.getElementById("fileContents").innerHTML = reader.result
-  	fileContents = reader.result 
-  return fileContents, filename
+	document.getElementById("fileContents").innerHTML = "File Loaded"
+	fileContents = reader.result 
+	return fileContents, filename
   };
 }
 
 function logFile(fileContents,filename,convert){
-	console.log("Filename",filename)
-	console.log(fileContents)
-	convert(fileContents)
+  console.log("Filename",filename)
+  console.log(fileContents)
+  convert(fileContents)
 }
 let dataList;
 function convert(fileContents){
@@ -33,24 +33,48 @@ function convert(fileContents){
 }
 var points = "No file loaded yet.";
 var items = "No Items Generated"
-function parseCSV(fileContents,dataList){
-	console.log(typeof fileContents)
+var cicles = []
+function parseCSV(fileContents,dataList,mymap){
+	//console.log(typeof fileContents)
 	points = fileContents.split("\n")
-	console.log(points)
-	console.log(typeof points)
+	//console.log(points)
+	//console.log(typeof points)
 	points.forEach((element,i) => {
 		points[i] = element.split(",");
-		console.log(element);
+		//console.log(element);
 		if (element.length===0){
 			console.log("Empty")
 			points.pop()
 		}
 	})
+	points.forEach((element,i) => {
+		element.splice(1,1)
+		element.splice(3,4)
+		if (!(i===0)){
+		L.circle([element[1], element[2]], {radius: 0.5,
+							  color: 'red',
+							  fillColor: 'red',
+    						  fillOpacity: 1}).addTo(mymap);
+		}
+	});
 	console.log(points)
+}
+var mymap="";
+function recentreMap(points,mymap,sum){
+	var sumLat = sum(points[0])
+	console.log(sumLat)
+	}
+
+}
+
+function sum(data){
+	for(i;i<data.length,i++){
+		console.log(i)
+	}
 }
 
 function loadMap(){
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	mymap = L.map('mapid').setView([51.505, -0.09], 13);
 	L.tileLayer('https://tile.thunderforest.com/{id}/{z}/{x}/{y}.png?apikey={accessToken}', {
 		attribution: 'Map data &copy; <a href="https://www.thunderforest.com/">ThunderForest</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		maxZoom: 18,
