4277f3d301cb9867e170f9d1e0bf45b8216b6e2c
M <matthewbye01@gmail.com>
2020-07-13 12:12:56 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -5,14 +5,17 @@ function hide(){
 	document.getElementById("Date").innerHTML = ""
 }
 
+var filename = ""
+var fileContents = ""
+
 function fileAdded(){
   var file = document.getElementById("fileInput").files[0];
-  var filename = file.name
+  filename = file.name
   var reader = new FileReader();
   reader.readAsText(file)
   reader.onload = function(){
   document.getElementById("demo").innerHTML = reader.result
-  var fileContents = reader.result 
+  fileContents = reader.result 
   return fileContents, filename
   };
 }
